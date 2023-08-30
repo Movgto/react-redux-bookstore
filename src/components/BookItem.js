295596bfdca75ece5ee0455d89d22b0b1b71839c
GUMARO MONROY VAZQUEZ <gumaro.monroyv@uanl.edu.mx>
2023-08-30 16:05:35 -0600

    Add book item and form components

---
+++ src/components/BookItem.js
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { remove } from '../features/books/bookSlice';
+
+const BookItem = ({ book }) => {
+  const dispatch = useDispatch();
+  const removeBook = (id) => {
+    dispatch(remove(id));
+  };
+
+  return (
+    <div className="book-item" key={book.id} id={(`book${book.id}`)}>
+      <h3>{book.title}</h3>
+      <h3>{book.author}</h3>
+      <button type="button" onClick={() => removeBook(book.id)}>Delete</button>
+    </div>
+  );
+};
+
+BookItem.propTypes = {
+  book: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default BookItem;
